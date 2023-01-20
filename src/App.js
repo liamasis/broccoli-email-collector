0ab12bd87bc69e137a7404e4c6328418c56743db
liamasis <liamcalebob@gmail.com>
2023-01-20 11:56:09 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Text, Flex } from "@chakra-ui/react";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  function Filters() {
+    const basicBoxStyles = {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      textAlign: "center",
+      boxSize: "250px",
+      color: "white",
+      textShadow: "0 0 20px black",
+      fontWeight: "bold",
+      fontSize: "20px",
+      px: 4,
+      background:
+        "url(https://picsum.photos/id/1080/200/300) center/cover no-repeat",
+    };
+    return (
+      <Flex flexWrap="wrap" gap="24px" justifyContent="space-evenly">
+        {/* adding filter property to the element */}
+        <Box sx={basicBoxStyles} filter="grayscale(80%)">
+          Box with Filter
+        </Box>
+        {/* adding blur property to the element */}
+        <Box sx={basicBoxStyles} filter="auto" blur="2px">
+          Box with Blur
+        </Box>
+        {/* adding brightness property to the element */}
+        <Box sx={basicBoxStyles} filter="auto" brightness="40%">
+          Box with Brightness
+        </Box>
+      </Flex>
+    );
+  }
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Input } from "@chakra-ui/react";
+function Login() {
+  return (
+    <div>
+      <Input />
+    </div>
+  );
+}
+
+export default Login;
