be97c10264bb811aac796ccc52bd42dff15bf142
liamasis <liamcalebob@gmail.com>
2023-01-20 16:43:41 +1100

    buttons and state

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,23 @@
 import React from "react";
-import { Input } from "@chakra-ui/react";
+import { Input, Button } from "@chakra-ui/react";
+import { useState } from "react";
 function Login() {
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [confirmEmail, setConfirmEmail] = useState("");
   return (
     <div>
-      <Input />
+      <form action="">
+        <Input
+          placeholder="full name"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Input placeholder="email" />
+        <Input placeholder="confirm-email" />
+        <Button />
+      </form>
     </div>
   );
 }
---
+++ src/components/Popup.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+} from "@chakra-ui/react";
+import Login from "./Login";
+function Popup() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody>
+            <Login />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="grey" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+export default Popup;
