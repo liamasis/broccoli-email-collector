03364581d68f58c6a521558cf4f6fe05ee738305
liamasis <liamcalebob@gmail.com>
2023-02-13 22:01:30 +1100

    why

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "../App.css";
 function Navbar() {
-  return <div className="navBar">Header</div>;
+  return <div className="navBar">Header | </div>;
 }
 
 export default Navbar;
