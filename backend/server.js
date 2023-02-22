645c529faac8ce1302f47e084a08925d78028d65
liamasis <liamcalebob@gmail.com>
2023-02-22 13:21:06 +1100

    converting to mern so i can run auth server

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/api/goals", (req, res) => {
+  res.json(message: 'get bread');
+});
+
+app.listen(port, () => console.log(`we out on here on port ${port} g.`));
