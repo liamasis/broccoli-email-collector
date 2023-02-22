1789230b37b1dcebbdad370bbd8835edbad9493e
liamasis <liamcalebob@gmail.com>
2023-02-22 14:06:45 +1100

    why

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.get("/api/goals", (req, res) => {
-  res.json(message: 'get bread');
+  res.json({ message: "get bread" });
 });
 
 app.listen(port, () => console.log(`we out on here on port ${port} g.`));
