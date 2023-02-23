445b6f461fbc3298929d05fedcd11f8fc4446f4f
liamasis <liamcalebob@gmail.com>
2023-02-23 23:27:59 +1100

    why god why

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-const app = express();
+const app= express();
 
 app.get("/api/goals", (req, res) => {
   res.json(message: 'get bread');
