9e7999c0957298213bc66eaa68eb4996ac71111b
liamasis <liamcalebob@gmail.com>
2023-02-27 15:09:40 +1100

    error handling

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,33 +1,36 @@
-
 // @desc    Get Goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = (req, res) => {
-    res.status(200).json({ message: 'get bread' });
+  res.status(200).json({ message: "get bread" });
 };
 
 // @desc    Set Goals
 // @route   POST /api/goals
 // @access  Private
 const setGoals = (req, res) => {
-    res.status(200).json({ message: 'get milk' });
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text file");
+  }
+  res.status(200).json({ message: "get milk" });
 };
 // @desc    Update Goals
 // @route   PUT /api/goals
 // @access  Private
 const updateGoals = (req, res) => {
-    res.status(200).json({ message: `Update goal ${req.params.id}` });
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
 };
 // @desc    Delete Goals
 // @route   DELETE /api/goals
 // @access  Private
 const deleteGoals = (req, res) => {
-    res.status(200).json({ message: `Delete goal ${req.params.id}` });
+  res.status(200).json({ message: `Delete goal ${req.params.id}` });
 };
 
 module.exports = {
-    getGoals,
-    setGoals,
-    deleteGoals,
-    updateGoals,
+  getGoals,
+  setGoals,
+  deleteGoals,
+  updateGoals,
 };
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,10 @@ const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
-const router = express;
 
-app.use('/api/goals', require('./routes/goalRoutes'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", require("./routes/goalRoutes"));
 
 app.listen(port, () => console.log(`we out on here on port ${port} g.`));
